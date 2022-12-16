8cb5c6671e4b210a62cbd5aba5f6a0fc1364dcd2
HalloJo <jorikvanruiswijk@gmail.com>
2022-12-16 10:08:19 +0100

    Set up header component, add assets

--- app/public/wp-content/themes/coolmat/header.php
+++ app/public/wp-content/themes/coolmat/header.php
@@ -31,32 +31,10 @@
 
 		<header id="masthead" class="site-header">
 			<div class="site-branding">
-				<?php
-                the_custom_logo();
-                if (is_front_page() && is_home()):
-                ?>
-				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-						<?php bloginfo('name'); ?>
-					</a></h1>
-				<?php else:
-                ?>
-				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-						<?php bloginfo('name'); ?>
-					</a></p>
-				<?php
-                endif;
-                $coolmat_description = get_bloginfo('description', 'display');
-                if ($coolmat_description || is_customize_preview()):
-                ?>
-				<p class="site-description">
-					<?php echo $coolmat_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+				<img src="<?php bloginfo('template_url'); ?>/assets/coolmat_logo.svg" alt="">
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-					<?php esc_html_e('Primary Menu', 'coolmat'); ?>
-				</button>
 				<?php
                 wp_nav_menu(
                 	array(
@@ -66,4 +44,7 @@
                 );
                 ?>
 			</nav><!-- #site-navigation -->
+			<div class="language-select">
+				KOR | ENG
+			</div>
 		</header><!-- #masthead -->
